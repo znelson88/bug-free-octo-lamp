a19768e4a63f3dc837811596708270385e4a221e
znelson88 <znelson88@gmail.com>
2026-06-19 12:32:50 -0700

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello World! 🎉 Your Worker is live.", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
